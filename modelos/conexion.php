ed0e5e89ba45b4ae08dd07c51856d4e8b9a509e6
Ruben Ortiz <rubenortizg@gmail.com>
2019-11-26 15:01:16 -0500

    pdo update

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,7 +4,7 @@ class Conexion {
 
   static public function conectar(){
 
-    $link = new PDO("mysql:host=localhost;dbname=sac","root","");
+    $link = new PDO("mysql:host=localhost;dbname=alegrias_sac","alegrias_sac","disturbio2006");
 
     $link->exec("set names utf8");
 
